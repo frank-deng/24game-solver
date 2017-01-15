fadb078e781a508d228870028747b2d51d681216
frank-deng <13382084679@163.com>
2017-01-15 13:12:12 +0800

    Test case show how many pairs of 4 numbers have answer for goals from 1 to 99

--- test.js
+++ test.js
@@ -1,3 +1,37 @@
 var solve24game = require('./24game-solver.min');
-console.log(solve24game(1, 1, 12, 1));
+
+var enumNumbers = function(count, min, max, func){
+	var nums = new Array();
+	var result = new Array();
+	for (var i = 0; i < count; i++) {
+		nums[i] = min;
+	}
+	while (nums[count-1] <= max) {
+		if (typeof(func) == 'function') {	
+			func.apply(func, nums);
+		}
+		nums[0]++;
+		for (var i = 0; i < count-1 && nums[i] > max; i++) {
+			nums[i] = min;
+			nums[i+1]++;
+		}
+		for (var i = 0; i < count-1; i++) {
+			if (nums[i] < nums[i+1]) {
+				for (var j = 0; j <= i; j++) {
+					nums[j] = nums[i+1];
+				}
+			}
+		}
+	}
+}
+for (var goal = 1; goal <= 99; goal++) {
+	var count = 0;
+	enumNumbers(4, 1, 13, function(a,b,c,d){
+		var result = solve24game(a,b,c,d,goal);
+		if (result.length > 0) {
+			count++;
+		}
+	});
+	console.log(goal+'\t'+count);
+}
 
