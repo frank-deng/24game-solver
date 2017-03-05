70cfffc2431d6424506e806fd2fa354ca0bbb72b
frank-deng <13382084679@163.com>
2017-03-05 20:01:53 +0800

    Use precompiled js code to speed up calculation

--- testexpr.js
+++ testexpr.js
@@ -24,18 +24,30 @@ var enumNumbers = function(count, min, max, func){
 		}
 	}
 }
+var oper = ['+', '-', '*', '/'];
+var simpleSolvertemplates = [
+	'n[0]%sn[1]%sn[2]%sn[3]',
+	'(n[0]%sn[1])%sn[2]%sn[3]',
+	'n[0]%s(n[1]%sn[2])%sn[3]',
+	'n[0]%sn[1]%s(n[2]%sn[3])',
+	'(n[0]%sn[1]%sn[2])%sn[3]',
+	'n[0]%s(n[1]%sn[2]%sn[3])',
+	'(n[0]%sn[1])%s(n[2]%sn[3])',
+];
+var simpleSolverExprs = new Array();
+for (var is1 = 0; is1 < 4; is1++) {
+	for (var is2 = 0; is2 < 4; is2++) {
+		for (var is3 = 0; is3 < 4; is3++) {
+			for (var i = 0; i < simpleSolvertemplates.length; i++) {
+				var t = simpleSolvertemplates[i];
+				simpleSolverExprs.push(new Function('n', 'return '+t.replace('%s',oper[is1]).replace('%s',oper[is2]).replace('%s',oper[is3])));
+			}
+		}
+	}
+}
+
 var simpleSolver = function(a,b,c,d,goal){
-	var templates = [
-		'%d%s%d%s%d%s%d',
-		'(%d%s%d)%s%d%s%d',
-		'%d%s(%d%s%d)%s%d',
-		'%d%s%d%s(%d%s%d)',
-		'(%d%s%d%s%d)%s%d',
-		'%d%s(%d%s%d%s%d)',
-		'(%d%s%d)%s(%d%s%d)',
-	];
-	var n = [a,b,c,d];
-	var oper = ['+', '-', '*', '/'];
+	var n = [a,b,c,d], arr = new Array(4);
 	for (var in1 = 0; in1 < 4; in1++) {
 		for (var in2 = 0; in2 < 4; in2++) {
 			if (in1 == in2) {
@@ -46,19 +58,11 @@ var simpleSolver = function(a,b,c,d,goal){
 					continue;
 				}
 				in4 = 6-in1-in2-in3;
-				for (var is1 = 0; is1 < 4; is1++) {
-					for (var is2 = 0; is2 < 4; is2++) {
-						for (var is3 = 0; is3 < 4; is3++) {
-							for (var i = 0; i < templates.length; i++) {
-								expr = templates[i]
-									.replace('%d',n[in1]).replace('%d',n[in2]).replace('%d',n[in3]).replace('%d',n[in4])
-									.replace('%s',oper[is1]).replace('%s',oper[is2]).replace('%s',oper[is3])
-								result = eval('('+expr+')');
-								if (Math.abs(result - goal) < 0.0000001) {
-									return expr;
-								}
-							}
-						}
+				for (var i = 0; i < simpleSolverExprs.length; i++) {
+					var calc_func = simpleSolverExprs[i];
+					arr[0] = n[in1]; arr[1] = n[in2]; arr[2] = n[in3]; arr[3] = n[in4];
+					if (Math.abs(goal - calc_func(arr)) < 0.0000001) {
+						return true;
 					}
 				}
 			}
@@ -68,19 +72,23 @@ var simpleSolver = function(a,b,c,d,goal){
 }
 
 console.log('Test for missing expressions.');
-console.log('This test will last for 7.5 hours.');
+console.log('This test will last for 11 minutes.');
 console.log('Press Ctrl-C to cancel.');
 console.log('');
 for (var goal = 0; goal <= 99; goal++) {
 	var count = 0;
 	enumNumbers(4, 1, 13, function(a,b,c,d){
 		var result = solve24game(a,b,c,d,goal);
-		var result2 = simpleSolver(a,b,c,d,goal);
+		try {
+			var result2 = simpleSolver(a,b,c,d,goal);
+		} catch(e) {
+			result2 = e;
+		}
 		if (result.length > 0 && result2 !== false) {
 			count++;
 		} else if (result.length == 0 && result2 === false) {
 		} else {
-			console.log('Missing expression: goal='+goal+' result='+result+' result_simple'+result2);
+			console.log('Missing expression: goal='+goal+' nums='+a+','+b+','+c+','+d);
 		}
 	});
 	console.log(goal+'\t'+count);
