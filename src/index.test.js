4368dc3c9e03fc3f357f242dfa50ee40e9edd175
frank-deng <13382084679@163.com>
2021-08-17 16:02:52 +0800

    Updated main test cases

--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,19 @@
 import solve24game from './index';
-import Calc from 'expression-calculator/src';
 
+function calcStr(str){
+    return new Function(`return ${str};`)();
+}
 describe('24game test',function(){
     it('1,2,3,4',function(){
-        let calc=new Calc();
         for(let item of solve24game(1,2,3,4,24)){
-            if(Math.abs(calc.compile(item).calc()-24)>0.01){
+            if(Math.abs(calcStr(item)-24)>0.01){
                 throw new Error(`${item} != 24`);
             }
         }
     });
     it('8,8,3,3',function(){
-        let calc=new Calc();
         for(let item of solve24game(8,8,3,3,24)){
-            if(Math.abs(calc.compile(item).calc()-24)>0.01){
+            if(Math.abs(calcStr(item)-24)>0.01){
                 throw new Error(`${item} != 24`);
             }
         }
